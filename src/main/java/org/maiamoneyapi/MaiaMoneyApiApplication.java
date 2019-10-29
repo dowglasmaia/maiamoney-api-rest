df75f8bfa22c442cd8e53e4cae88b2b0d146f769
dowglasmaia <dowglasmaia@gmail.com>
2019-10-28 21:43:53 -0300

    Start Project

---
+++ src/main/java/org/maiamoneyapi/MaiaMoneyApiApplication.java
@@ -0,0 +1,13 @@
+package org.maiamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiaMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiaMoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maiamoneyapi/MaiaMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maiamoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiaMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
